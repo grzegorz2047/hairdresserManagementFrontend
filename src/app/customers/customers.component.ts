9466d01f1da0eac39333af7bc98efafa5438017a
Grzegorz <grzegorz2047@gmail.com>
2019-01-10 00:32:49 +0100

    Wstępne przygotowanie listy klientów

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer } from '../customer'
+import { CUSTOMERS } from '../mock-customers';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
   styleUrls: ['./customers.component.css']
 })
-export class CustomersComponent implements OnInit {
-  
-  customer: Customer = {
-    id: 1,
-    firstname: 'Jan',
-    lastname: 'Kowalski',
-    phoneNumber: '693785343',
-    appointment:  new Date()
 
-  };
-  
+export class CustomersComponent implements OnInit {
+
+  customers = CUSTOMERS;
+  // customer: Customer = {
+  //   id: 1,
+  //   firstname: 'Jan',
+  //   lastname: 'Kowalski',
+  //   phoneNumber: '693785343',
+  //   appointment:  new Date()
+
+  // };
+  selectedCustomer: Customer;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onSelect(customer: Customer): void {
+    this.selectedCustomer = customer;
   }
 
 
